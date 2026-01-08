65f69e7d9f93b90579972b372d625378f50f32ed
syedriad <imriad55@gmail.com>
2026-01-09 02:20:35 +0500

    first commit

---
+++ backend/routes/workouts.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const {
+  createWorkout,
+  getAllWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/WorkoutController");
+
+const router = express.Router();
+
+// get all workouts
+router.get("/", getAllWorkouts);
+
+// get a single workout
+
+router.get("/:id", getWorkout);
+
+// POST a new workout
+router.post("/", createWorkout);
+
+// DELETE a new workout
+router.delete("/:id", deleteWorkout);
+
+// UPDATE a new workout
+router.patch("/:id", updateWorkout);
+
+module.exports = router;
